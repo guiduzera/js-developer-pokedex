05b8af2757f609826589e4f319dcbad87034d84f
Guilherme Carvalho <guigui.carvalho15@gmail.com>
2023-08-22 16:57:50 -0300

    FEAT: Implementando a lógica para modal de deta;hes extras

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,14 @@ function convertPokemonToLi(pokemon) {
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
+
+            <div class="moreDetails">
+                <span class="height">Height: ${pokemon.height}</span>
+                <span class="weight">Weight: ${pokemon.weight}</span>
+                <ul class="abilities">
+                    ${pokemon.abilities.map((ability) => `<li class="ability">${ability}</li>`).join('')}
+                </ul>
+            </div>
         </li>
     `
 }
@@ -27,6 +35,17 @@ function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map(convertPokemonToLi).join('')
         pokemonList.innerHTML += newHtml
+        const pokeItemMoreDetails = document.querySelectorAll('.moreDetails')
+        pokeItemMoreDetails.forEach((pokeItemMoreDetail) => {
+            pokeItemMoreDetail.classList.add('hidden')
+        })
+        const pokeItems = document.querySelectorAll('.pokemon')
+        pokeItems.forEach((pokeItem) => {
+            pokeItem.addEventListener('click', (event) => {
+                const pokeItemMoreDetails = event.currentTarget.querySelector('.moreDetails')
+                pokeItemMoreDetails.classList.toggle('hidden')
+            })
+        })
     })
 }
 
